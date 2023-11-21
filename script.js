8908ab23d6ebe16425764352efd6e4a5fd6450bc
martin20032001 <107205123+martin20032001@users.noreply.github.com>
2023-11-21 18:11:38 +0100

    d

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const video = document.getElementById('video')
 const startButton = document.getElementById('start-button')
+const dd = document.getElementById("test1")
 
-
+var canvas2 = document.getElementById('gca');
+var running = false;
+let intervallid = 0
+var created = false;
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -23,24 +27,53 @@ function startVideo() {
 // Event-Listener für das Abspielen des Videos hinzufügen
 video.addEventListener('play', () => {
   // Canvas-Element erstellen und zum Body hinzufügen
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
+  if (!created){
+  created =true
+  canvas = faceapi.createCanvasFromMedia(video)
+  canvas2.replaceWith(canvas)
+
   // Größe des Anzeigebereichs festlegen
   const displaySize = { width: video.offsetWidth, height: video.offsetHeight }
   // Die Dimensionen des Canvas an die Anzeigegröße anpassen
   faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
+  intervallid= setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
+  }
+  
 })
 
+function stopVideo() {
+  let video = document.querySelector('video'); // Holen des Videoelements
+  let stream = video.srcObject; // Zugriff auf den Stream
+
+  if (stream) {
+    let tracks = stream.getTracks(); // Holen der Tracks des Streams
+    tracks.forEach(track => track.stop()); // Stoppen aller Tracks
+
+    // Leeren des Video-Elements und des Stream-Objekts
+    video.srcObject = null;
+  }
+}
+
 startButton.addEventListener('click', function(){
-  startVideo();
+  if (!running){
+    running=true
+    startVideo();
+    dd.style.display="block";
+  } else{
+    
+    dd.style.display="none";
+    running = false;
+    stopVideo()
+  }
+  
 }); 
 
 
