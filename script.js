b9a66efd0ca5a6542b986df0cb1210fb5752eea4
martin20032001 <107205123+martin20032001@users.noreply.github.com>
2023-11-27 23:05:49 +0100

    s

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 const faceTrackButton = document.getElementById("faceTrack");
 const trackSite = 'track.html';
 
@@ -7,15 +8,38 @@ const emotionSite = 'showemotions.html';
 const emotionGameButton = document.getElementById("emotionGame");
 const emotionGameSite = 'emotionGame.html';
 
+var isChromium = window.chrome;
+var winNav = window.navigator;
+var vendorName = winNav.vendor;
+var isOpera = typeof window.opr !== "undefined";
+var isIEedge = winNav.userAgent.indexOf("Edg") > -1;
+var isIOSChrome = winNav.userAgent.match("CriOS");
 
+//Wenn Google Chrome nicht verwendet wird soll ein alert erscheinen.
+if (isIOSChrome) {
+   //Google Chrome auf iOS
+} else if(
+  isChromium !== null &&
+  typeof isChromium !== "undefined" &&
+  vendorName === "Google Inc." &&
+  isOpera === false &&
+  isIEedge === false
+) {
+   //Google CHrome sonst
+} else { 
+   // kein Google Chrome 
+   alert("Please use Google Chrome to ensure full functionality of all features. Using a different browser may result in the webcam not functioning properly");
+}
+
+//Funktionen der Buttons
 faceTrackButton.addEventListener('click', function(){
-    window.location.href = trackSite;
+  window.location.href = trackSite;
 }); 
 
 emotionTrackButton.addEventListener('click', function(){
-  window.location.href = emotionSite;
+window.location.href = emotionSite;
 }); 
 
 emotionGameButton.addEventListener('click', function(){
-  window.location.href = emotionGameSite;
+window.location.href = emotionGameSite;
 }); 
